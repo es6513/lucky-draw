f9959e32911d0b55baebfb4fd3e82804da8cd58b
Jason <es6513@gmail.com>
2020-12-11 00:30:38 +0800

    set timer by useRef&useState

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,86 +1,84 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { useSelector, useDispatch } from "react-redux";
 import { setCountdownSeconds } from "../../redux/modules/timer/actions";
 import { SECONDS_PER_MINUTE } from "../../utils/timeConstants";
 import Input from "../Input";
 import Button from "../Button";
+var countDownTimer;
 
 function Timer() {
-  const [localInputTime, setLocalInputTime] = useState(0);
-  const isInitRender = useRef(true);
+  const [countdownSeconds, setCountdownSeconds] = useState(0);
   const refCountTime = useRef();
-  //Store
-  const dispatch = useDispatch();
-  const setStoreCountdownTime = (countdownMinutes) => {
-    const payload = {
-      countdownSeconds: parseFloat(countdownMinutes * SECONDS_PER_MINUTE),
-    };
-    dispatch(setCountdownSeconds(payload));
-  };
+  const countdownTimer = useRef();
 
-  const storeCountdownSeconds = useSelector(
-    (state) => state.timerReducer.countdownSeconds
-  );
-  //Side Effect
-
-  useEffect(() => {
-    if (isInitRender.current) {
-      isInitRender.current = false;
-      return;
-    }
-    refCountTime.current = storeCountdownSeconds;
-    const countDown = (timerId) => {
-      if (refCountTime.current > 0) {
-        refCountTime.current--;
-      } else {
-        refCountTime.current = 0;
-      }
-      dispatch(setCountdownSeconds({ countdownSeconds: refCountTime.current }));
-      if (refCountTime.current === 0) {
-        console.log("end up");
-        clearInterval(timerId);
-      }
-    };
-    const timerId = setInterval(() => {
-      countDown(timerId);
-    }, 1000);
-    console.log(timerId);
-
-    return () => {
-      console.log("clear", timerId);
-      clearInterval(timerId);
-    };
-  }, [localInputTime]);
   //Timer Form
   const { register: formRegister, formState, handleSubmit } = useForm({
     mode: "all",
     criteriaMode: "all",
   });
 
-  const startCountDown = (data) => {
-    const { countdownMinutes } = data;
-    setStoreCountdownTime(countdownMinutes);
-    setLocalInputTime(countdownMinutes);
+  const setTimerSeconds = (data) => {
+    let { inputMinutes } = data;
+    let inputSeconds = parseFloat(inputMinutes * SECONDS_PER_MINUTE);
+    refCountTime.current = inputSeconds;
+    setCountdownSeconds(inputSeconds);
   };
+
+  const startCountdown = () => {
+    const countDown = (timerId) => {
+      // clearInterval(countdownTimer.current);
+      console.log("state seconds", countdownSeconds);
+      console.log("ref seconds", refCountTime.current);
+
+      if (refCountTime.current > 0) {
+        refCountTime.current--;
+      } else {
+        refCountTime.current = 0;
+      }
+      if (refCountTime.current <= 0) {
+        console.log("end up");
+        clearInterval(timerId);
+      }
+      setCountdownSeconds(refCountTime.current);
+    };
+    countdownTimer.current = setInterval(() => {
+      countDown(countdownTimer.current);
+    }, 1000);
+  };
+
+  const handleCountdownProcess = (data) => {
+    clearInterval(countdownTimer.current);
+    setTimerSeconds(data);
+    startCountdown();
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(countdownTimer.current);
+    };
+  }, []);
 
   //Format Time
   const renderFormatCountdownTime = () => {
-    let renderMinutes = Math.floor(storeCountdownSeconds / SECONDS_PER_MINUTE);
-    let renderSeconds = storeCountdownSeconds % SECONDS_PER_MINUTE;
+    let renderMinutes = Math.floor(countdownSeconds / SECONDS_PER_MINUTE);
+    let renderSeconds = countdownSeconds % SECONDS_PER_MINUTE;
     renderMinutes = String(renderMinutes).padStart(2, 0);
     renderSeconds = String(renderSeconds).padStart(2, 0);
     const mmssFormatTime = `${renderMinutes}:${renderSeconds}`;
     return mmssFormatTime;
   };
+  const remainTime = useMemo(() => renderFormatCountdownTime(), [
+    countdownSeconds,
+  ]);
   return (
     <div>
-      <form onSubmit={handleSubmit(startCountDown)}>
-        <Input inputName="countdownMinutes" useFormRef={formRegister} />
+      <form onSubmit={handleSubmit(handleCountdownProcess)}>
+        <Input inputName="inputMinutes" useFormRef={formRegister} />
         <span>分鐘</span>
         <Button type="submit">設定</Button>
       </form>
-      {renderFormatCountdownTime()}
+      {remainTime}
     </div>
   );
 }
--- src/containers/TimeControl.js
+++ src/containers/TimeControl.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeadTitle from "../components/HeadTitle";
 import Timer from "../components/Timer";
 
@@ -7,10 +7,16 @@ const { css } = config;
 const { ROOT_CLASS } = css;
 
 function TimeControl() {
+  const [showTimer, setShowTimer] = useState(true);
+  const removeTimer = () => {
+    setShowTimer((showTimer) => !showTimer);
+  };
   return (
     <div className={`${ROOT_CLASS}__time-control`}>
       <HeadTitle headTag="h3">抽獎時間</HeadTitle>
-      <Timer />
+      {showTimer ? <Timer /> : null}
+
+      <button onClick={removeTimer}>Remove Timer</button>
     </div>
   );
 }
