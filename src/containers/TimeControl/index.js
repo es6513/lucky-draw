05c204b3772bfb535bb3a7e1bb5053dafa3dd386
Jason <es6513@gmail.com>
2020-12-15 06:49:05 +0800

    modify hint and input rult

--- src/containers/TimeControl/index.js
+++ src/containers/TimeControl/index.js
@@ -3,9 +3,9 @@ import { useDispatch } from "react-redux";
 import classnames from "classnames/bind";
 import { toggleShowResult } from "store/modules/result/actions";
 import { setLuckyPerson } from "store/modules/attendLists/actions";
+import { COUNTDOWN_MAX_MINUTES } from "utils/timeConstants";
 import HeadTitle from "components/HeadTitle";
 import Timer from "components/Timer";
-
 import styles from "./style.module.scss";
 import { config } from "config";
 
@@ -38,15 +38,13 @@ function TimeControl() {
       <div className={cx("timer-control-hint")}>
         倒數時間說明:
         <br />
-        1. 可接受 0 ~ 150,119,987,579,016 分鐘;
+        1. 可接受 0 ~ {COUNTDOWN_MAX_MINUTES}之正數 (ex:2 ,10, 0.5) ;
         <br />
-        2. 可接受任何正數 (ex:2 ,10, 0.5);
+        2. 含特殊字元, 負數, 空格或其他錯誤格式等會無效 (ex: +3, 02, -1, 00.5);
         <br />
-        3. 含特殊字元, 負數, 空格或其他錯誤格式等會無效 (ex: +3, 02, -1, 00.5);
+        3. 倒數秒數為四捨五入後的結果;
         <br />
-        4. 倒數秒數為四捨五入後的結果;
-        <br />
-        5. 若整體時間不足1秒,則以1秒計算;
+        4. 若整體時間不足1秒,則以1秒計算;
       </div>
     </div>
   );
