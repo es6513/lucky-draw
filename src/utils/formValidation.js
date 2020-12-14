fd488f80df8cf03141b99d49dbe47e02cd7163c7
Jason <es6513@gmail.com>
2020-12-15 03:26:05 +0800

    adjust input validation

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -1,17 +1,22 @@
+import { SECONDS_PER_MINUTE } from "./timeConstants";
+
+const inputFormat = /^(?!0\d)(?:\d+)(?:[.,]\d+)?$/;
+
 export const inputMinutesValidation = {
   required: true,
-  max: Number.MAX_SAFE_INTEGER / 60,
-  validate: {
-    number: (value) => !isNaN(value),
-    bePositive: (value) => value > 0,
-  },
   pattern: /^(?!0\d)(?:\d+)(?:[.,]\d+)?$/,
+  validate: {
+    isOverMax: (value) =>
+      !isNaN(value) && value < Number.MAX_SAFE_INTEGER / SECONDS_PER_MINUTE,
+    isNumber: (value) => !isNaN(value),
+    isPositive: (value) => value > 0,
+  },
 };
 
 export const inputErrorMessages = {
   required: "請輸入倒數時間",
-  max: "超過最大可倒數時間",
-  number: "請輸入數字",
-  bePositive: "需輸入大於0之數字",
   pattern: "格式錯誤,請參考說明",
+  isOverMax: "超過最大可倒數時間",
+  isNumber: "請輸入數字",
+  isPositive: "需輸入大於0之數字",
 };
